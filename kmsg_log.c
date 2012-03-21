40f60aa243898b05bf4b5381c493e685c263251a
Austen Dicken <cvpcsm@gmail.com>
2012-03-21 01:53:40 -0500

    general code cleanup
    remove goto (yuck)
    add check for memory allocation issues
    add some extra comments
    clean code layouts

--- kmsg_log.c
+++ kmsg_log.c
@@ -10,35 +10,37 @@
 
 #define PROCFS_NAME "kmsg_log"
 
-static ssize_t procfile_write(struct file *file, const char __user *buffer, size_t count,
-		loff_t *data) {
-	int ret;
-	char* text;
-	char* ptr;
-	char* tmp;
+static ssize_t procfile_write ( struct file *file, const char __user *buffer,
+		size_t count, loff_t *data ) {
+	char *text, *ptr, *tmp;
 
 	text = (char *)vmalloc((count + 1) * sizeof(char));
-	if ( copy_from_user(text, buffer, count) ) {
-		ret = -EFAULT;
-		goto end;
+	if ( text == NULL ) {
+		// out of memory, bail
+		return -ENOMEM;
 	}
+
+	if ( copy_from_user(text, buffer, count) ) {
+		// free our memory and bail. something went wrong
+		vfree(text);
+		return -EFAULT;
+	}
+	// add a null-terminator just to be safe
 	text[count] = '\0';
 
 	// tokenize on newline characters
 	ptr = text;
 	tmp = strsep(&ptr, "\r\n");
-	while (tmp != NULL)
-	{
-		if (strlen(tmp) > 0)
+	while ( tmp != NULL ) {
+		// ignore empty lines
+		if ( strlen(tmp) > 0 )
 			printk(KERN_INFO "%s\n", tmp);
 
 		tmp = strsep(&ptr, "\r\n");
 	}
-	ret = count;
 
-end:
 	vfree(text);
-	return ret;
+	return count;
 }
 
 static struct proc_dir_entry *procfile;
@@ -48,10 +50,10 @@ static const struct file_operations procfile_fops = {
 	.write = procfile_write,
 };
 
-static int __init kmsg_log_init(void) {
+static int __init kmsg_log_init ( void ) {
 	procfile = proc_create(PROCFS_NAME, 0200, NULL, &procfile_fops);
 
-	if (procfile == NULL) {
+	if ( procfile == NULL ) {
 		remove_proc_entry(PROCFS_NAME, NULL);
 		printk(KERN_ALERT "Error: Could not initialize /proc/%s\n",
 			PROCFS_NAME);
@@ -62,7 +64,7 @@ static int __init kmsg_log_init(void) {
 	return 0;
 }
 
-static void __exit kmsg_log_exit(void) {
+static void __exit kmsg_log_exit ( void ) {
 	remove_proc_entry(PROCFS_NAME, NULL);
 	printk(KERN_INFO "/proc/%s removed\n", PROCFS_NAME);
 }
